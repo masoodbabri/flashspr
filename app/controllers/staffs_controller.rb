5fdefd0aba6e9a551263ec504b1f0f2d9e054e4a
An Nguyen <an@innotac.com>
2016-09-01 19:27:05 -0400

    Initial

---
+++ app/controllers/staffs_controller.rb
@@ -0,0 +1,31 @@
+class StaffsController < ApplicationController
+  PAGE_SIZE = 10
+
+  def index
+
+    # existing index method
+
+    @page = (params[:page] || 0).to_i
+    if params[:keywords].present?
+      @keywords = params[:keywords]
+      staff_search_term = StaffSearchTerm.new(@keywords)
+      @staffs = Staff.where(
+          staff_search_term.where_clause,
+          staff_search_term.where_args).
+          order(staff_search_term.order).
+          offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
+    else
+      @staffs = []
+    end
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @staffs }
+    end
+  end
+
+  def show
+    @staff = Staff.find(params[:id])
+  end
+
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,34 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+1_000.times do |i|
+  Staff.create!(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      title: Faker::Name.title,
+      email: Faker::Internet.user_name + i.to_s +
+          "@#{Faker::Internet.domain_name}")
+end
+
+Staff.create(
+         first_name: 'An',
+         last_name: 'Nguyen',
+         title: 'Co-Founder',
+         email: 'an@innotac.com')
+
+Staff.create(
+    first_name: 'David',
+    last_name: 'Sachdev',
+    title: 'Co-Founder',
+    email: 'david@innotac.com')
+
+Staff.create(
+    first_name: 'Ayden',
+    last_name: 'Nguyen',
+    title: 'Intern',
+    email: 'feilong601@gmail.com')
