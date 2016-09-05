1d69f2e26275bb77f497f41cf1bc54311b116ec7
David Sachdev <david@innotac.com>
2016-09-05 15:14:31 -0400

    Issues with Rspec

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -4,7 +4,7 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
   #test "should get index" do
     # Need to flush out the issue with this
     #get dashboard_index_url
-    assert_response :success
+    #assert_response :success
   #end
 
 end
