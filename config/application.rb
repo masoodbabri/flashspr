913278b24725cad76f92caac00c8a3428c65752a
David Sachdev <david@innotac.com>
2016-09-05 14:22:18 -0400

    Adding in additional environments

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,21 @@ module FlashSPR
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #Web Console is activated in the dev environment. This is
+    #usually a mistake. To ensure it's only activated in development
+    #mode, move it to the development group of your Gemfile:
+
+    #gem 'web-console', group: :development
+
+    #If you still want to run it the dev environment (and know
+    #what you are doing), put this in your Rails application
+    #configuration:
+
+    #config.web_console.development_only = false
+    config.web_console.development_only = false
+
   end
 end
+
+
