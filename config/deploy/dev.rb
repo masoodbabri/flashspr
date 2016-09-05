e730f3da9f83fd75e96dec0138a76e0280cb27f6
David Sachdev <david@innotac.com>
2016-09-05 19:14:24 -0400

    Adding in config for dev environment

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-
+server 'flash-dev-rails1', user: 'jenkins', roles: %w{app db web}
 
 
 # role-based syntax
