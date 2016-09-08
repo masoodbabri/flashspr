461608fb3623419b8801b1131fe454ae4124c682
An Nguyen <an@innotac.com>
2016-09-08 14:01:01 -0400

    new front page

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -21,6 +21,14 @@ class StaffsController < ApplicationController
       format.html {}
       format.json { render json: @staffs }
     end
+
+    $curUserStaff = Staff.find_by_email(current_user.email)
+    @curUser = current_user.email
+    @staff = Staff.find_by_email(@curUser)
+    @last10Kudos = Kudo.last(5).reverse;
+    @sentKudos = Kudo.where(:sender => @curUser)
+    authorize :staff, :login_process?
+
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908164842) do
+ActiveRecord::Schema.define(version: 20160908171057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20160908164842) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_awards_on_name", unique: true, using: :btree
+  end
+
+  create_table "badges", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "point_level"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "categories", force: :cascade do |t|
